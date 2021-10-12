830da3562048ba3dbbd5b18f4246c9a0bdbe8bd6
sushidesu <hugemerge@gmail.com>
2021-10-12 17:55:28 +0900

    create roomgit a

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import styles from './App.module.css'
 import { database } from "./plugins/firebase"
-import { ref, get } from "firebase/database"
+import { ref, get, set } from "firebase/database"
+import { customAlphabet } from "nanoid"
 
 function App() {
   const [key, setKey] = useState("")
@@ -18,14 +19,35 @@ function App() {
     }
   }
 
+  const generateKey = () => {
+    const custom = customAlphabet("abcdefghijklmnopqrstuvwxyz", 8)
+    return custom()
+  }
+  const [value, setValue] = useState("")
+  const create = async () => {
+    const newKey = generateKey()
+    const roomRef = ref(database, `room/${newKey}`)
+    await set(roomRef, value)
+    window.alert(`ROOM: ${newKey}`)
+  }
+
   return (
     <div>
       <h1 className={styles.root}>share dayo</h1>
       <div>
+        <p>探す</p>
         <input onBlur={(e) => {
           setKey(e.target.value)
         }} />
         <button onClick={search}>OK</button>
+      </div>
+      <hr />
+      <div>
+        <p>作る</p>
+        <input onBlur={(e) => {
+          setValue(e.target.value)
+        }} />
+        <button onClick={create}>CREATE</button>
       </div>
     </div>
   )
